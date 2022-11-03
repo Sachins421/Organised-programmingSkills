a573c4a24058e766338bcc013ea96ad4d0dbfd56
Sachin Shukla <sachin.shukla@misterspex.de>
2022-11-03 17:20:22 +0100

    App:SimpleCalcuator

---
+++ C#/SimpleCalculator/SimpleCalculator/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Calculator
+    {
+        public int InputValue1;
+        public int Inputvalue2;
+        public string action;
+
+        public Calculator(int aInputValue, int bInputValue, string aAction)
+        {
+            InputValue1 = aInputValue;
+            Inputvalue2 = bInputValue;
+            action = aAction;
+        }
+
+        public void Getsum()
+        {
+            if (action == "+")
+            {
+                Console.WriteLine(InputValue1 + Inputvalue2);
+            }
+            else if (action == "-")
+            {
+                Console.WriteLine(InputValue1 - Inputvalue2);
+            }
+            else if (action == "/")
+            {
+                Console.WriteLine(InputValue1 / Inputvalue2);
+            }
+            else if (action == "*")
+            {
+                Console.WriteLine(InputValue1 * Inputvalue2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input."); 
+            }
+            
+        }
+    }
+}
---
+++ C#/SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first value:");
+            int value1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter airthmatic expression:");
+            string action = Console.ReadLine();
+            
+            Console.Write("Enter second value:");
+            int value2 = Convert.ToInt32(Console.ReadLine());
+            
+            Calculator calc = new Calculator(value1, value2, action);
+            
+            try
+            {
+                calc.Getsum();
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
