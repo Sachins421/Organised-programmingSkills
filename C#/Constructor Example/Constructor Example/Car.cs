a742c27c1e51f2e3b8e771be84fd5fdd76792a9e
Sachin Shukla <sachin.shukla@misterspex.de>
2022-11-05 12:29:02 +0100

    Constructor Example

---
+++ C#/Constructor Example/Constructor Example/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Example
+{
+    class Car
+    {
+        public string model;
+        public string color;
+        public int year;
+        int month = 2; // if var is not public then it can be accessible through method
+        public Car(string pmodel, string pcolor, int pyear)
+        {
+            model = pmodel;
+            color = pcolor;
+            year = pyear;
+        }
+        public int getmonth()
+        {
+            return month;
+        }
+    }
+
+}
---
+++ C#/Constructor Example/Constructor Example/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // first method without parameter
+            //Car featur = new Car();
+            //featur.model = "Mustang";
+            //featur.color = "red";
+            //featur.year = 2005;
+
+            //Console.WriteLine(featur.model);
+            //Console.WriteLine(featur.color);
+            //Console.WriteLine(featur.year);
+
+            // Second method using constructor
+            // constructor is used initialized default value to the variable when object is created for the class, costructor is called itself when object is created regardless of calling
+            // it.
+            Car featur = new Car("Mustang","red",2006);
+
+            Console.WriteLine(featur.model);
+            Console.WriteLine(featur.color);
+            Console.WriteLine(featur.year);
+            Console.WriteLine(featur.getmonth());
+            Console.ReadLine();
+
+        }
+    }
+}
