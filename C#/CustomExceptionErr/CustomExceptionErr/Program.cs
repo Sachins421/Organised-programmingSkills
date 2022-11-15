0bc38b610dcdd83ea684d58a45dc8ff02917b395
Sachin Shukla <sachin.shukla@misterspex.de>
2022-11-15 17:55:58 +0100

    String builder and CustomException

---
+++ C#/CustomExceptionErr/CustomExceptionErr/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExceptionErr
+{
+    public class NegativeException : ApplicationException
+    {
+        public NegativeException(string error) : base(error)
+        {
+
+        }
+    }
+    public class NumberZeroException : ApplicationException
+    {
+        public NumberZeroException(string error) : base(error)
+        {
+
+        }
+    }
+
+    internal class Program
+    {
+        public void calc(int a, int b)
+        {
+            if (a < 0 || b < 0)
+            {
+                throw new NegativeException("Nos can not be in negative..");
+
+            }
+            if (a == 0 || b == 0)
+            {
+                throw new NumberZeroException("Nos can not be zero..");
+
+            }
+            else
+            {
+                double c = a + b;
+                Console.WriteLine("Sum is {0}",c);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two nos..");
+            int a = Convert.ToInt32( Console.ReadLine());
+            int b = Convert.ToInt32( Console.ReadLine());
+
+            Program pr = new Program();
+
+            try
+            {
+                pr.calc(a, b);
+            }
+            catch (NegativeException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            catch (NumberZeroException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("sum up done.");
+            }
+        }
+    }
+}
---
+++ C#/ValidationWithStringBuilder/ValidationWithStringBuilder/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationWithStringBuilder
+{
+    internal class Program
+    {
+        class Emplyee
+        {
+            public int empno;
+            public string empName;
+            public string DepName;
+            public int basic;
+        }
+
+        class DataValidation
+        {
+            public StringBuilder validate(Emplyee employ)
+            {
+                StringBuilder sb = new StringBuilder();
+                bool flag = true;
+                if (employ.empno <= 2)
+                {
+                    sb.AppendLine("Emp no. must be with at least 4 digit \n");
+                    flag = false;
+                }
+                if (employ.empName.Length <= 5)
+                {
+                    sb.Append("Emp Name must with 6 char \n");
+                    flag = false;
+                }
+                if (!employ.DepName.Equals("NAV"))
+                {
+                    sb.Append("Dept must be NAV \n");
+                    flag = false;
+                }
+                if (flag)
+                {
+                    Console.WriteLine("All validation are passed!");
+                }
+                return sb;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Emplyee employ = new Emplyee();
+            Console.WriteLine("Enter the emp no.");
+            employ.empno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the emp name.");
+            employ.empName = Console.ReadLine();
+            Console.WriteLine("Enter the department.");
+            employ.DepName = Console.ReadLine();
+            Console.WriteLine("Enter the basic.");
+            employ.basic = Convert.ToInt32(Console.ReadLine());
+
+            DataValidation dv = new DataValidation();
+            Console.WriteLine(dv.validate(employ));
+        }
+    }
+}
