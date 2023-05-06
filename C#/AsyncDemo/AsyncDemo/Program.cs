a4cf816cfc276574525a4d75aab88d78f83556d1
Sachin Shukla <sachin.shukla@misterspex.de>
2023-05-06 11:32:56 +0200

    Async Demo

---
+++ C#/AsyncDemo/AsyncDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncDemo
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Task<String> result = Process();
+
+            AnotherProcess();
+
+            String value = await result;
+
+            Console.WriteLine(value);
+
+            Console.ReadLine();
+        }
+
+        static async Task<String> Process()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Thread 1 started");
+
+                await Task.Delay(500);
+
+                Console.WriteLine("Thread 1 finished");
+            }
+            
+
+            return "Process completed";
+        }
+
+        static void AnotherProcess()
+        {
+            Console.WriteLine("Thread 2 started");
+            
+
+            Console.WriteLine("Thread 2 finished");
+        }
+
+    }
+}
