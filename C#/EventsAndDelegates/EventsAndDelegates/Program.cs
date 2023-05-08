f437a34ac1d8a4c9bd87842719126e7725729212
Sachin Shukla <sachin.shukla@misterspex.de>
2023-05-08 13:30:07 +0200

    More code added related to delegate

--- C#/EventsAndDelegates/EventsAndDelegates/Program.cs
+++ C#/EventsAndDelegates/EventsAndDelegates/Program.cs
@@ -1,11 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace EventsAndDelegates
 {
+    public class EnrolledArgs : EventArgs
+    {
+        public short EnrolledYear {get; set;}
+        public string EnrolledMonth { get; set;} 
+    }
+
+    public class StudentGradeArgs : EventArgs
+    {
+        public int StudentGradeYear { get; set; }
+        public string StudentGradeMonth { get; set; }
+    }
+
+    public class Student
+    {
+        // the sender
+        //a class that contains any arguments about the event being raised.
+        public delegate void EnrollmentHandler(object sender, EnrolledArgs args); //Delegate second para should be matched to EventArgs class
+
+        public event EnrollmentHandler OnEnrolled; // The return type from an event is a delegate of type EventHandler that defines these two arguments.
+
+        public void Enroll()
+        {
+            OnEnrolled(this, new EnrolledArgs { EnrolledYear = (short)DateTime.Now.Year, EnrolledMonth = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(DateTime.Now.Month)});
+        }
+
+        public event EventHandler<StudentGradeArgs> OnStudentGrade;
+        
+        public void Grade(decimal grade)
+        {
+            if (grade > 70) //(OnStudentGrade != null)  
+            {
+                OnStudentGrade(this, new StudentGradeArgs { StudentGradeMonth = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(DateTime.Now.Month), 
+                    StudentGradeYear = (short)DateTime.Now.Year});
+            }
+               
+        }
+    }
     public class File
     {
         public static void Main(string[] args)
@@ -22,8 +62,19 @@ namespace EventsAndDelegates
 
             encryptFile.Encrypt(dataFile);
 
+            var student = new Student();
+
+            //student.OnEnrolled += (sender, args) => Console.WriteLine("Enrolled for the month and year : "+ args.EnrolledMonth, args.EnrolledYear);
+            student.OnEnrolled += (sender, args) => Console.WriteLine($"Enrolled for the month and year : {args.EnrolledMonth} {args.EnrolledYear}");
+            student.Enroll();
+
+            student.OnStudentGrade += (sender, args) => Console.WriteLine($"Enrolled grade for the month and year : {args.StudentGradeMonth} {args.StudentGradeYear}");
+            student.Grade(80);
+            
             Console.ReadLine();
 
         }
     }
+
+   
 }
