26b1c29307a337db9d0eba3deddab807577de2a1
Sachin Shukla <sachin.shukla@misterspex.de>
2023-05-28 10:35:21 +0200

    Interface demo

---
+++ C#/InterfaceDemo/InterfaceDemo/DisplayLogger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+{
+    public class DisplayLogger : ILogger
+    {
+        public int _ErrorCount = 0;
+        public void LogError(string error)
+        {
+            Console.WriteLine("Error was supplied: " + error);
+            _ErrorCount++;
+        }
+        //public int ErrorCount { get { return 5; } }
+        //public int ErrorCounter { get { return _ErrorCount; } }
+
+        public int ErrorCount { get { return _ErrorCount; } }
+    }
+
+    public class DBLogger : ILogger
+    {
+        public int _ErrorCount = 0;
+        void ILogger.LogError(string error) // Explicit implementation of interface :It is also possible to HIDE the interface so
+                                            // that it is only visible when the class is explicitly cast to the interface type.
+        {
+            Console.WriteLine("Explicit implementation error called: " + error);
+            _ErrorCount++;
+        }
+
+        public int ErrorCount { get { return _ErrorCount; } }
+
+    }
+
+    class NewThing : ILogger2
+    {
+        public void DoSomethingElse()
+        {
+            Console.WriteLine("Something else?");
+        }
+
+        //public void DoTheThing()
+        //{
+        //    Console.WriteLine("I did the thing!");
+        //}
+
+    }
+}
---
+++ C#/InterfaceDemo/InterfaceDemo/ILogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+{
+    public interface ILogger
+    {
+        //Members in interface are public by default
+        void LogError(string error);    
+        int ErrorCount { get; }
+    }
+
+    ///////////////////////////Default Implementations////////////////////////////////////////////////////////////////////////////////
+
+    public interface IDoAThing
+    {
+        void DoTheThing();
+    }
+    public interface ILogger2 : IDoAThing
+    {
+        void DoSomethingElse();
+        void IDoAThing.DoTheThing()
+        {
+            Console.WriteLine("This is my default thing");
+        }
+    }
+}
