2c481ae1effa3a2588e76bf52ebbdb1bbfc7957c
Sachin Shukla <sachin.shukla@misterspex.de>
2023-05-28 10:33:32 +0200

    new classes fo exception

---
+++ C#/CustomExceptionErr/CustomExceptionErr/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExceptionErr
+{
+    public class Calculator
+    {
+        public void calc(int a, int b)
+        {
+            if (a < 0 || b < 0)
+            {
+                throw new NegativeException("Nos can not be in negative..");
+
+            }
+            if (a == 0 || b == 0)
+            {
+                throw new NumberZeroException("Nos can not be zero..");
+
+            }
+            else
+            {
+                double c = a + b;
+                Console.WriteLine("Sum is {0}", c);
+            }
+        }
+        public decimal DivideHandled(decimal arg1, decimal? arg2)
+        {
+
+            /**/
+            if (arg2 == null) throw new BetterDivisionException("Cannot divide by null") { Dividend = arg1, Divisor = arg2 };
+            if (arg2.Value == 0) throw new BetterDivisionException("Cannot divide by zero") { Dividend = arg1, Divisor = arg2 }; // if keep empty string this one called
+            /**/
+
+            return arg1 / arg2.Value;
+
+        }
+    }
+}
---
+++ C#/CustomExceptionErr/CustomExceptionErr/CustomExceptions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExceptionErr
+{
+    public class BetterDivisionException : System.Exception
+    {
+        //There are four constructor signatures that you SHOULD implement in your custom exception class,
+        //and then you can implement any other properties or methods you would like. These are not required, but are considered good practice:
+        public BetterDivisionException() : base("Unable to divide by zero") { }  // this one called 
+        public BetterDivisionException(string message) : base(message) { }
+        public BetterDivisionException(string message, System.Exception inner) : base(message, inner) { }
+
+        // A constructor is needed for serialization when an
+        // exception propagates from a remoting server to the client.
+        protected BetterDivisionException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+
+        // Remember, division is Dividend / Divisor
+
+        public decimal? Dividend { get; set; }
+        public decimal? Divisor { get; set; }
+    }
+    public class NegativeException : Exception // Microsoft now recommends Exception class to create a custom exception class instead of ApplicationException.
+    {
+        public NegativeException(string error) : base(error)
+        {
+
+        }
+    }
+    public class NumberZeroException : ApplicationException
+    {
+        public NumberZeroException(string error) : base(error)
+        {
+
+        }
+    }
+
+}
